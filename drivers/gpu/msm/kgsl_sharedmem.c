c6423acedc4eeccdbdc0e0cb66d436c7b15b2f02
zymphad <zymphad@live.com>
2016-07-01 11:18:49 -0400

    more fixes

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -617,17 +617,10 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 
 	len = size;
 
-	// only care about size for tracing
-	trace_kgsl_sharedmem_page_alloc(size, page_size, align);
-
 	while (len > 0) {
 		struct page *page;
 
-		/* don't waste space at the end of the allocation*/
-		if (len < page_size)
-			page_size = PAGE_SIZE;
-
-		page = kgsl_heap_alloc(page_size);
+		page = alloc_page(GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
 
 		if (page == NULL) {
 			/*
@@ -646,14 +639,12 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 			goto done;
 		}
 
-		/*
-		 * We need to confirm the actual page size returned by kgsl_heap_alloc.
-		 * It is likely not the same as what we asked for.
-  		 */
-		page_size = PAGE_SIZE << compound_order(page);
+		ptr = kmap_atomic(page);
+		dmac_flush_range(ptr, ptr + PAGE_SIZE);
+		kunmap_atomic(ptr);
 
-		sg_set_page(&memdesc->sg[sglen++], page, page_size, 0);
-  		len -= page_size;
+		sg_set_page(&memdesc->sg[sglen++], page, PAGE_SIZE, 0);
+		len -= PAGE_SIZE;
 	}
 
 	memdesc->sglen = sglen;
